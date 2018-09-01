7b2b747c49e663e1771e493138e1c55981d4c00d
Rebecca Liu <yliu1033@bloomberg.net>
2018-08-31 18:08:47 -0700

    add kafka propterties class

---
+++ src/main/java/com/rekekkaxi/AsynchronousProducer.java
@@ -0,0 +1,5 @@
+package com.rekekkaxi;
+
+public class AsynchronousProducer {
+
+}
---
+++ src/main/java/com/rekekkaxi/KafkaProperties.java
@@ -0,0 +1,38 @@
+package com.rekekkaxi;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class KafkaProperties {
+  private Properties prop;
+
+  public KafkaProperties(Properties prop) {
+    this.prop = prop;
+  }
+
+  public KafkaProperties() {
+    prop = new Properties();
+  }
+
+  public KafkaProperties(String configPath) {
+    prop = new Properties();
+    InputStream inputStream;
+    try {
+      inputStream = new FileInputStream(configPath);
+      prop.load(inputStream);
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+
+  public Properties getProperties() {
+    return this.prop;
+  }
+
+  public void addConfig(String key, String value) {
+    prop.put(key, value);
+  }
+
+}
